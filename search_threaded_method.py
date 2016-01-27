055321428a813ad7089458de3669781fb5810078
VOX <jonasvox.2014@gmail.com>
2016-01-27 20:59:13 +0100

    Update search_threaded_method.py

--- search_threaded_method.py
+++ search_threaded_method.py
@@ -4,9 +4,12 @@ import binascii
 from math import factorial
 from decimal import Decimal, getcontext
 import datetime
-import thread
+import threading
 
 string = "test"
+numThreads = 2
+threads = []
+
 abc = "abcdefghijklmnopqrstuvwxyz"
 dec = ""
 for i in string:
@@ -21,25 +24,35 @@ passed = 0
 def calc(seek, buffer_size): #Imports Pi
     pi_dict = open("dict.txt", 'r')
     pi_dict.seek(seek)
-    chunk = pi_dict.read( buffer_size )
+    chunk = pi_dict.read( int(buffer_size) )
     pi_dict.close()
     return chunk
 
-str_pi = str(calc(passed, digits))
 
 print("Started: "+str(datetime.datetime.now())+"\n")
-print("Digits_in, Search_Status, String_to_Decimal")
+print("Digits_in, Search_Status, String_to_Decimal, Thread")
 print("Searching every "+str(hop_amount)+" digits.")
 
-while(found == False):
-    if(str(dec) in str_pi):
-        found = True
-    print(str(digits)+", "+str(found)+", "+str(dec))
-    digits=digits+hop_amount
-    str_pi = str(calc(passed, digits))
-    passed = passed+hop_amount
-encoded = [str_pi.find(str(dec)), str_pi.find(str(dec))+len(str(dec))]
+def search(thread, dec):
+    while(found == False):
+        if(str(dec) in str_pi):
+            found = True
+        print(str(digits)+", "+str(found)+", "+str(dec)+", Thread-"+str(thread))
+        digits=digits+hop_amount
+        str_pi = str(calc(passed*thread+thread, digits*thread+thread))
+        print(str_pi)
+        passed = (passed+hop_amount)*thread
+        
+thread = 0
+while thread < numThreads:
+    thread=thread+1
+    print("Started Thread-"+str(thread))
+    str_pi = str(calc(passed*thread+thread, digits*thread+thread))
+    t = threading.Thread(target=calc, args=(thread, dec,))
+    threads.append(t)
+    t.start()
 
+encoded = [str_pi.find(str(dec)), str_pi.find(str(dec))+len(str(dec))]
 print("\n\nEncode Format: [start_in_pi, end_in_pi] ")
 print(string+" to "+str(encoded))
 
