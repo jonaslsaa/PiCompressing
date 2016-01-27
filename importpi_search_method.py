edd3b29085b24cb220504e85d9ac4acc8336d16b
VOX <jonasvox.2014@gmail.com>
2016-01-27 20:26:08 +0100

    better encoded
    hopefully

--- importpi_search_method.py
+++ importpi_search_method.py
@@ -4,24 +4,14 @@ import binascii
 from math import factorial
 from decimal import Decimal, getcontext
 import datetime
+import thread
 
-#STR TO BIN
-string = "vox"
-binary = bin(int.from_bytes(string.encode(), 'big'))
-
-#BIN TO STR
-#n = int(binary, 2)
-#n.to_bytes((n.bit_length() + 7) // 8, 'big').decode()
-
-#BIN TO DEC
-raw_dec = int(binary, 2)
-
-#DEC TO BIN
-#binary = bin(dec)
-
-#Pre-Compression
-dec = int(raw_dec) #Nothing yet (GOAL: to compress to fewest ammount of bits)
-
+string = "test"
+abc = "abcdefghijklmnopqrstuvwxyz"
+dec = ""
+for i in string:
+    dec=dec+str(abc.find(i)+10)
+binary=bin(int.from_bytes(str(dec).encode(), 'big'))
 
 found = False
 hop_amount = 100000+len(str(dec))
@@ -40,6 +30,7 @@ str_pi = str(calc(passed, digits))
 print("Started: "+str(datetime.datetime.now())+"\n")
 print("Digits_in, Search_Status, String_to_Decimal")
 print("Searching every "+str(hop_amount)+" digits.")
+
 while(found == False):
     if(str(dec) in str_pi):
         found = True
@@ -48,6 +39,7 @@ while(found == False):
     str_pi = str(calc(passed, digits))
     passed = passed+hop_amount
 encoded = [str_pi.find(str(dec)), str_pi.find(str(dec))+len(str(dec))]
+
 print("\n\nEncode Format: [start_in_pi, end_in_pi] ")
 print(string+" to "+str(encoded))
 
